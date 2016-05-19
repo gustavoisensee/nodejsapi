b7074d99771ce05f8bb99aa8642c76b90adc47dd
Gustavo Isensee <gustavo.isensee@gmail.com>
2016-05-19 17:43:58 -0300

    Added spdy/http2 and certificate

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
+'use strict';
+
 const express    = require('express');
+const spdy       = require('spdy');
 const app        = express();
 const bodyParser = require('body-parser');
+const fs         = require('fs');
 const morgan     = require('morgan');
 const port       = (process.env.PORT || 3000);
 const router     = require('./app/routers/start');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use(morgan('dev')); // use morgan to log requests to the console
 
+// Use morgan to log requests to the console
+app.use(morgan('dev')); 
+
+// Define routers
 app.use('/api', router);
-app.listen(port);
+
+// SSL options
+var options = {
+    key: fs.readFileSync('./app/security/localhost.key'),
+    cert: fs.readFileSync('./app/security/localhost.cert')
+};
+
+spdy.createServer(options, app).listen(port);
--- app/routers/start.js
+++ app/routers/start.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const router = require('express').Router();
 const jwt    = require('jsonwebtoken');
 const config = require('../../config');
